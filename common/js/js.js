fe3f7a9fe64644a30aa7b285f8e90545294fdb3e
Moussa <moussainssa@outlook.fr>
2021-05-18 12:54:32 +0200

    Footer + Modal + Table

---
+++ common/js/js.js
@@ -0,0 +1,25 @@
+//  Lire une vidéo
+function lireAudio(record_id) {
+    $('.audiosRecord').each(function(){
+        $(this)[0].pause()
+    })
+    $('#audio'+record_id)[0].play()
+}
+// SweetAlertMessage
+function messageAlert(typealerte, message) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: typealerte,
+        title: message
+    })
+}
